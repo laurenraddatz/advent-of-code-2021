4b35762539cb435dd5ad91fc807b0920ea84f09e
Lauren Raddatz <lraddatz@squareup.com>
2021-12-02 12:22:17 -0500

    refactor day one

--- src/1/index.js
+++ src/1/index.js
@@ -2,29 +2,7 @@ import { readFileSync } from "fs"
 
 const input = readFileSync('src/1/input.txt').toString().split('\n').map((a) => parseInt(a))
 
-const partOne = () => {
-  let increases = 0
+const getIncreases = (indexSize) => input.reduce((count, curr, i, acc) => i >= indexSize && curr > acc[i - indexSize] ? count + 1 : count, 0)
 
-  for (let i = 1; i < input.length; i++) {
-    if (input[i] > input[i - 1]) increases++
-  }
-
-  return increases
-}
-
-const partTwo = () => {
-  let increases = 0;
-
-  for (let i = 3; i < input.length; i++) {
-    const currSum = input[i - 2] + input[i - 1] + input[i]
-    const prevSum = input[i - 3] + input[i - 2] + input[i - 1]
-
-    if (currSum > prevSum) increases++
-  }
-
-  return increases
-}
-
-
-console.log('part one: ', partOne())
-console.log('part two: ', partTwo())
+console.log('part one: ', getIncreases(1))
+console.log('part two: ', getIncreases(3))
