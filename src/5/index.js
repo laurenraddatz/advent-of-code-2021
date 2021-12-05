972215c7d8ce27f71673215a577a33ea5fbfb032
Lauren Raddatz <lraddatz@squareup.com>
2021-12-05 02:11:29 -0500

    day five

--- src/5/index.js
+++ src/5/index.js
@@ -2,42 +2,7 @@ import { readFileSync } from 'fs'
 
 const input = readFileSync('src/5/input.txt').toString().split('\n')
 
-const partOne = () => {
-  const grid = new Map()
-
-  const lines = input.map((line) => line.split(' -> '))
-
-  for (let i = 0; i < lines.length; i++) {
-    const [from, to] = lines[i]
-    const [x0, y0] = from.split(',').map((a) => parseInt(a))
-    const [x1, y1] = to.split(',').map((a) => parseInt(a))
-
-    if (x0 === x1) {
-      const [start, end] = [y0, y1].sort((a, b) => a - b)
-      let count = start
-
-      while (count <= end) {
-        const coord = `${x0}, ${count}`
-        grid.set(coord, (grid.get(coord) ?? 0) + 1)
-        count++
-      }
-    } else if (y0 === y1) {
-      const [start, end] = [x0, x1].sort((a, b) => a - b)
-      let count = start
-
-      while (count <= end) {
-        const coord = `${count}, ${y0}`
-        grid.set(coord, (grid.get(coord) ?? 0) + 1)
-        count++
-      }
-    }
-  }
-
-  const vals = grid.values()
-  return [...vals].reduce((acc, a) => a > 1 ? acc + 1 : acc, 0) // 7644
-}
-
-const partTwo = () => {
+const dayFive = (diagonals = false) => {
   const lines = input.map((line) => line.split(' -> '))
 
   const grid = new Map()
@@ -65,7 +30,7 @@ const partTwo = () => {
         grid.set(coord, (grid.get(coord) ?? 0) + 1)
         count++
       }
-    } else if (Math.abs(x0 - x1) === Math.abs(y0 - y1)) {
+    } else if (diagonals && Math.abs(x0 - x1) === Math.abs(y0 - y1)) {
       // y = mx + b
       const slope = (y1 - y0) / (x1 - x0)
       const b = y0 - x0 * slope
@@ -82,8 +47,8 @@ const partTwo = () => {
   }
 
   const vals = grid.values()
-  return [...vals].reduce((acc, a) => a > 1 ? acc + 1 : acc, 0) // 18627
+  return [...vals].reduce((acc, a) => a > 1 ? acc + 1 : acc, 0)
 }
 
-console.log('part one: ', partOne())
-console.log('part two: ', partTwo())
+console.log('part one: ', dayFive()) // 7644
+console.log('part two: ', dayFive(true)) // 18627
